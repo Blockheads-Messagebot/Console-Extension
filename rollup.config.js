410394f6b82089f6d62524b9730610ec05d480b5
Bibliofile <Bibliofilegit@gmail.com>
2017-10-11 17:15:53 -0600

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,11 @@
 import typescript from 'rollup-plugin-typescript2'
 import string from 'rollup-plugin-string'
+import { existsSync, unlinkSync } from 'fs'
+
+for (let file of ['history', 'index']) {
+    if (existsSync(`./${file}.d.ts`)) unlinkSync(`./${file}.d.ts`)
+}
+
 
 export default {
     input: 'src/index.ts',
